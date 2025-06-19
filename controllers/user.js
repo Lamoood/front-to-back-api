fac3b051532894338aa7554a99059ca51b6af16b
Lamoood <pimonmuk@gmail.com>
2025-06-19 10:15:28 +0700

    update-v.3

--- controllers/user.js
+++ controllers/user.js
@@ -1,33 +1,42 @@
-import { createError } from "../utils/createError.js"
+import prisma from "../config/prisma.js";
+import { createError } from "../utils/createError.js";
 
-export const listUser = (req, res, next) => {
+export const listUser = async (req, res, next) => {
   try {
-    //1.Check Email
-    if(false){
-      createError(400, "Email already exists!!!")
-    } else {
-      throw new Error("Password is Invalid!!!")
-    }
-      //code body
-    res.json({ message:"This is List All User"})
+    //check Email
+    const user = await prisma.user.findMany({
+      omit: {
+        password: true
+      }
+    });
+    console.log(user);
+
+    res.json({ message: "This is List All User", result: user });
   } catch (error) {
-    //next error
-    next(error)
+    next(error);
   }
-}
+};
+
+export const updateRoleUser = async (req, res, next) => {
+  try {
+    //1. Read params & body
+    const { id } = req.params
+    const { role } = req.body
+    console.log(id, role)
+    res.json({ message: "This is Update Role user" });
+  } catch (error) {}
+};
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    res.json({ message: "This is Delete user" });
+  } catch (error) {}
+};
 
 export const readUser = (req, res) => {
-  res.json({ message: "This is READ User"})
-}
+  res.json({ message: "This is Read User" });
+};
 
 export const createUser = (req, res) => {
-  res.json({ message : 'This is POST user'})
-}
-
-export const updateRoleUser = (req, res) => {
-  res.json({ message : 'This is UPDATE role user'})
-}
-
-export const deleteUser = (req, res) => {
-  res.json({ message : 'This is DELETE user'})
-}
+  res.json({ message: "This is POST user" });
+};
--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,7 +1,35 @@
+import { createError } from "../utils/createError.js";
+import jwt from "jsonwebtoken";
+
 export const authCheck = (req, res, next) => {
-  //code body
-  if(true){
-  console.log("This is middleware")
-next();
-}
-}
+  try {
+    //TO DO OVERVIEW
+    /*
+  1. Check header
+  2. Split token bearer
+  3. Verify JWT
+  */
+    const header = req.headers.authorization;
+    console.log('header',header)
+    if (!header) {
+      createError(401, "Token is missing");
+    }
+
+    //2. Split Token
+    const token = header.split(" ")[1];
+    console.log(token)
+
+    //3.Verify Token
+    jwt.verify(token, process.env.SECRET, (error, decode) => {
+      console.log(error);
+      console.log(decode);
+      if (error) {
+        createError(401, "Token is Invalid!!!");
+      }
+      req.user = decode
+      next()
+    });
+  } catch (error) {
+    next(error);
+  }
+};
